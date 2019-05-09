ae6def4475375ca57a9f88bfbb1c1be0145367f9
Rob Taylor <rtaylor@clixifix.co.uk>
2019-05-09 12:29:23 +0100

    added bootstrap and jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,42 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+const webpack = require('webpack')
+/**
+ * Automatically load modules instead of having to import or require them everywhere.
+ * Support by webpack. To get more information:
+ *
+ * https://webpack.js.org/plugins/provide-plugin/
+ * http://j.mp/2JzG1Dm
+ */
+environment.plugins.prepend(
+    'Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        jquery: 'jquery',
+        'window.jQuery': 'jquery'
+    })
+)
+
+
+
+/**
+ * To use jQuery in views
+
+ https://gist.github.com/andyyou/834e82f5723fec9d2dc021fb7b819517
+
+ yarn add expose-loader -D
+
+environment.loaders.append('expose', {
+    test: require.resolve('jquery'),
+    use: [{
+        loader: 'expose-loader',
+        options: '$'
+    }]
+})
+
+ */
+
 module.exports = environment
